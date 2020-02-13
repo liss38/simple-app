570bf2109485467db635ceb8f0829af184d1a9e3
Max Salnikov <liss38@yandex.ru>
2020-02-13 18:59:05 +0300

    ссылка на сводную задачу, туду лист

--- src/components/table-data/index.jsx
+++ src/components/table-data/index.jsx
@@ -13,19 +13,33 @@ const TableData = ({ tableData }) => {
 	};
 
 	return (
-		<div onDoubleClick={doubleClickHandle}>
-			Test TableData <br />
-			---------------------
-			<br />
-			<br />
-			{
-				tableDataContentMapper(tableData.content).map( (item) => <TableDataRow
-					key={item[0][`cellValue`]}
-					idDataRow={item[0][`cellValue`]}
-					dataRow={item}
-				/>)
-			}
-		</div>
+		<React.Fragment>
+			<div className="dable-data" onDoubleClick={doubleClickHandle}>
+				Test TableData <br />
+				---------------------
+				<br />
+				<br />
+				{
+					tableDataContentMapper(tableData.content).map( (item) => <TableDataRow
+						key={item[0][`cellValue`]}
+						idDataRow={item[0][`cellValue`]}
+						dataRow={item}
+					/>)
+				}
+			</div>
+
+			<div style={{width: `500px`, fontFamily: `Arial`}}>
+				<b>Как сделать типовые задачи в web приложении на React JS: (это для резюме условно, для наработки практики, выложить на гитхаб пэйджес)</b>
+				<ul>
+					<li><a href="https://habr.com/ru/post/346584/" target="_blank">https://habr.com/ru/post/346584/ (ознакомительная статья)</a></li>
+					<li><a href="https://avrylkov.github.io/react/" target="_blank">https://avrylkov.github.io/react/ (пример для "потыканья")</a></li>
+					<li><a href="https://github.com/avrylkov/react" target="_blank">https://github.com/avrylkov/react (исходники "примера для потыканья")</a></li>
+				</ul>
+
+				<h2>Сделать @TODO</h2>
+				темы: -"рефакторинг и упорядочивание"; -"table-data"; ...
+			</div>
+		</React.Fragment>
 	);
 };
 
